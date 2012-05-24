71bcac62ba36446472a8b402cee5c39d1df80fd0
Bill Durr <billyburly@gmail.com>
2012-05-24 13:25:38 -0400

    add retry loop

--- update-temp.py
+++ update-temp.py
@@ -16,20 +16,34 @@ import os
 #            print "hi"
 #            k = commands.getoutput("kill " + instance[1])
 
-ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=5)
-ser.write("g")
-data = ser.read(25)
+retry = 5
+data = None
+ser = serial.Serial('/dev/ttyUSB0', 9600, timeout = 1)
+
+while retry > 0:
+    retry -= 1
+    
+    ser.write("g")
+    data = ser.read(25)
+
+    if data != None and len(data) > 0:
+        retry += 1
+        break
+
+if retry == 0:
+    print "read failed"
+    exit()
+
 print data
 m = re.match("T: (-?\d*\.\d*); H: (\d*\.\d*);", data)
 if m:
     ret = rrdtool.update("/srv/http/temp-humid/temp-humid.rrd", "N:" + m.group(1) + ":" + m.group(2))
     if ret:
         print rrdtool.error()
-#    else:
-#        print "success"
 else:
     print "regex fail"
-    print data
+
+    
 #    else:
 #        ret = rrdtool.graph("/srv/http/temp-humid/temp-humid.png", "--start", "-1d", "--vertical-label=Temperature F / % Humidity",  "--width=600", "--height=200",
 # "DEF:temp=/srv/http/temp-humid/temp-humid.rrd:temperature:AVERAGE",
