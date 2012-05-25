e0cf90a74cc94982cb7c89c224c218f462cb543c
Bill Durr <billyburly@gmail.com>
2012-05-24 22:22:42 -0400

    use ethernet instead of serial

--- update-temp.py
+++ update-temp.py
@@ -1,63 +1,57 @@
 import sys
-import serial
 import rrdtool
 import re
 import commands
 import string
 import os
 import MySQLdb
+import telnetlib
 
+host = '192.168.1.80'
 retry = 5
+
 data = None
-ser = serial.Serial('/dev/ttyUSB0', 9600, timeout = 1)
+temp = None
+humid = None
 
 while retry > 0:
     retry -= 1
+    data = None
     
-    ser.write("g")
-    data = ser.read(25)
+    try:
+        tn = telnetlib.Telnet(host)
+        tn.write("f")
+
+        data = tn.read_until("\n")
+        (temp, humid) = data.rstrip("\n").split(",")
+    except:
+        print "telnet error"
+        data = None
 
     if data != None and len(data) > 0:
         retry += 1
         break
 
-if retry == 0:
-    print "read failed"
+if data == None:
+    print "data request failed"
     exit()
 
-print data
-m = re.match("T: (-?\d*\.\d*); H: (\d*\.\d*);", data)
-if m:
-	temp = m.group(1)
-	humid = m.group(2)
-	ret = rrdtool.update("/srv/http/temp-humid/temp-humid.rrd", "N:" + temp + ":" + humid)
-	if ret:
-		print rrdtool.error()
+if temp == "-9999":
+    temp = ""
+if humid == "-9999":
+    humid = ""
 
-	conn = MySQLdb.connect(host="localhost", user="smarthouse", passwd="***REMOVED***", db="smarthouse")
-	cursor = conn.cursor()
-	try:
-		cursor.execute("""INSERT INTO `point_data_float` (`ipid`, `timestamp`, `value`) VALUES (1, NOW(), %s), (2, NOW(), %s)""", (temp, humid))
-		conn.commit()
-	except:
-		print "err"
-		conn.rollback();
-	cursor.close()
-	conn.close()
-else:
-    print "regex fail"
+ret = rrdtool.update("/srv/http/temp-humid/temp-humid.rrd", "N:" + temp + ":" + humid)
+if ret:
+	print rrdtool.error()
 
-    
-#    else:
-#        ret = rrdtool.graph("/srv/http/temp-humid/temp-humid.png", "--start", "-1d", "--vertical-label=Temperature F / % Humidity",  "--width=600", "--height=200",
-# "DEF:temp=/srv/http/temp-humid/temp-humid.rrd:temperature:AVERAGE",
-# "DEF:humid=/srv/http/temp-humid/temp-humid.rrd:humidity:AVERAGE",
-# "AREA:humid#ccccff",
-# "LINE1:humid#5555ff:Humidity\\r",
-# "LINE2:temp#ee2200:Temperature F", 
-# "COMMENT:\\r",
-# "GPRINT:temp:LAST:Tempature \: %6.2lf %S F",
-# "GPRINT:humid:LAST:Humidity \: %6.2lf %S %%\\r")
-#        if ret:
-#            print rrdtool.error()
- 
+conn = MySQLdb.connect(host="localhost", user="smarthouse", passwd="", db="smarthouse")
+cursor = conn.cursor()
+try:
+	cursor.execute("""INSERT INTO `point_data_float` (`ipid`, `timestamp`, `value`) VALUES (1, NOW(), %s), (2, NOW(), %s)""", (temp, humid))
+	conn.commit()
+except:
+	print "err"
+	conn.rollback();
+cursor.close()
+conn.close()
